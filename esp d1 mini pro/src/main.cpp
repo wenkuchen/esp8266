89b9348e32a0122da581da5f4514c2f30c0b5399
Wayne Chen <wenkuchen@gmail.com>
2022-04-21 17:35:49 +0800

    commit header changes

--- esp d1 mini pro/src/main.cpp
+++ esp d1 mini pro/src/main.cpp
@@ -3,19 +3,18 @@
 #include <ESPAsyncWebServer.h>
 
 #include "config.h"
+#include "foo.h"
 
 void setup() {
   // put your setup code here, to run once:
   WiFi.mode(WIFI_AP_STA); 
-  WiFi.begin("WCK","23752755");
+  WiFi.begin(SSID,"23752755");
   
   Serial.begin(115200);
 	delay(20);
-  
-  //test();
 }
 
 void loop() {
-  
+   Serial.print(2,foo());
   // put your main code here, to run repeatedly:
 }
