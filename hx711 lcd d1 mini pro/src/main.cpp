0c87ec13b6b22b79dc007bf29ddd34c099d05277
Wayne Chen <wenkuchen@gmail.com>
2022-05-04 13:47:39 +0800

    adding scale lcd msg print function

--- hx711 lcd d1 mini pro/src/main.cpp
+++ hx711 lcd d1 mini pro/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "config.h"
 #include "init.h"
 #include <LiquidCrystal_I2C.h>
 #include "HX711.h" 
@@ -12,13 +13,22 @@ HX711 scale;
 // set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27,20,4);  
 
+void LcdMsgLine(int col, int row, String msg){ 
+  lcd.setCursor(0,row);
+  lcd.print("                "); // clear the row
+  lcd.setCursor(col,row);
+  lcd.println(msg);   
+}
+
 void setup() {
   Serial.begin(115200);
   esp8266_init();
 	delay(20);
   
   lcd.init(); // initialize the lcd 
-  lcd.backlight();
+  lcd.backlight();lcd.clear();
+  LcdMsgLine(2,0, "Scale Ready   ");
+  LcdMsgLine(1,1, WiFi.localIP().toString().c_str());
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 }
