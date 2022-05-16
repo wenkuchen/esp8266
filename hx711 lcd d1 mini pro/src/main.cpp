952ece3e0e32f7a659c8185f21694c94e6d2e8f8
Wayne Chen <wenkuchen@gmail.com>
2022-05-17 07:30:01 +0800

    wifiMgr lib and JSON lib added

--- hx711 lcd d1 mini pro/src/main.cpp
+++ hx711 lcd d1 mini pro/src/main.cpp
@@ -37,16 +37,21 @@ Serial.print("read: \t\t");
 Serial.println(scale.read()); // print a raw reading from the ADC
 
 Serial.print("read average: \t\t");
-Serial.println(scale.read_average(20)); // print the average of 20 readings from the ADC
+Serial.println(scale.read_average(20)); 
+// print the average of 20 readings from the ADC
 
 Serial.print("get value: \t\t");
-Serial.println(scale.get_value(5)); // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+Serial.println(scale.get_value(5)); 
+// print the average of 5 readings from the ADC minus the tare weight (not set yet)
 
 Serial.print("get units: \t\t");
-Serial.println(scale.get_units(5), 1); // print the average of 5 readings from the ADC minus tare weight (not set) divided
+Serial.println(scale.get_units(5), 1); 
+// print the average of 5 readings from the ADC minus tare weight (not set) divided
 // by the SCALE parameter (not set yet)
 
-scale.set_scale(2280.f); // this value is obtained by calibrating the scale with known weights; see the README for details
+scale.set_scale(2280.f); 
+// this value is obtained by calibrating the scale with known weights; 
+// see the README for details
 scale.tare(); // reset the scale to 0
 
 Serial.println("After setting up the scale:");
@@ -55,13 +60,16 @@ Serial.print("read: \t\t");
 Serial.println(scale.read()); // print a raw reading from the ADC
 
 Serial.print("read average: \t\t");
-Serial.println(scale.read_average(20)); // print the average of 20 readings from the ADC
+Serial.println(scale.read_average(20)); 
+// print the average of 20 readings from the ADC
 
 Serial.print("get value: \t\t");
-Serial.println(scale.get_value(5)); // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+Serial.println(scale.get_value(5)); 
+// print the average of 5 readings from the ADC minus the tare weight, set with tare()
 
 Serial.print("get units: \t\t");
-Serial.println(scale.get_units(5), 1); // print the average of 5 readings from the ADC minus tare weight, divided
+Serial.println(scale.get_units(5), 1); 
+// print the average of 5 readings from the ADC minus tare weight, divided
 // by the SCALE parameter set with set_scale
 
 Serial.println("Readings:");
