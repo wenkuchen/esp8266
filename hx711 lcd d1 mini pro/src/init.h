3e6f0afd075a87d70b24d6ea80c1936392e12009
Wen Ku Chen <49545014+wenkuchen@users.noreply.github.com>
2022-06-09 15:46:09 +0800

    Update init.h

--- hx711 lcd d1 mini pro/src/init.h
+++ hx711 lcd d1 mini pro/src/init.h
@@ -7,6 +7,8 @@ float RefKG = 2.0; // Reference weight as RefADC,BaseADC as empty
 
 enum WS_client_type_enum {SET_REF, SET_BASE, SET_REFKG, SET_UXTIME};
 char* WS_client_type_array[]={"SET_REF", "SET_BASE", "SET_REFKG", "SET_UXTIME"};
+int WS_client_type_array_len = 4;
 enum WS_server_type_enum {SET_REF_OK, SET_BASE_OK, SET_REFKG_OK, SET_UXTIME_OK, ON_CHG} ;
 char* WS_server_type_array[]={"SET_REF_OK", "SET_BASE_OK", "SET_REFKG_OK", "SET_UXTIME_OK", "ON_CHG"};
+int WS_server_type_array_len = 5;
 
