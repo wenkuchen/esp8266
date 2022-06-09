ca401f93bfd20da783b40da514e31af1fc5c96c4
Wen Ku Chen <49545014+wenkuchen@users.noreply.github.com>
2022-06-09 15:28:10 +0800

    Update init.h

--- hx711 lcd d1 mini pro/src/init.h
+++ hx711 lcd d1 mini pro/src/init.h
@@ -5,8 +5,8 @@ void esp8266_init();
 long CurrADC, RefADC, BaseADC; // hx711 scale variables
 float RefKG = 2.0; // Reference weight as RefADC,BaseADC as empty 
 
-enum WS_client_type {SET_REF, SET_BASE, SET_REFKG, SET_UXTIME};
+enum WS_client_type_enum {SET_REF, SET_BASE, SET_REFKG, SET_UXTIME};
 char* WS_client_type_array[]={"SET_REF", "SET_BASE", "SET_REFKG", "SET_UXTIME"};
-enum WS_server_type {SET_REF_OK, SET_BASE_OK, SET_REFKG_OK, SET_UXTIME_OK, ON_CHG} ;
+enum WS_server_type_enum {SET_REF_OK, SET_BASE_OK, SET_REFKG_OK, SET_UXTIME_OK, ON_CHG} ;
 char* WS_server_type_array[]={"SET_REF_OK", "SET_BASE_OK", "SET_REFKG_OK", "SET_UXTIME_OK", "ON_CHG"};
 
