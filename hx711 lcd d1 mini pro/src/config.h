76cefa6cd0443d78b907f3f4c27c9c10b955381c
Wen Ku Chen <49545014+wenkuchen@users.noreply.github.com>
2022-06-09 15:39:42 +0800

    Update config.h

--- hx711 lcd d1 mini pro/src/config.h
+++ hx711 lcd d1 mini pro/src/config.h
@@ -31,7 +31,7 @@
 #define DEF_SSID "WKC"
 #define DEF_PASSWORD "23752755"
 
-#define SERVER_HOST_NAME "esp_server"
+#define SERVER_HOST_NAME "scale_server"
 
 #define TCP_PORT 7050
 #define DNS_PORT 53
