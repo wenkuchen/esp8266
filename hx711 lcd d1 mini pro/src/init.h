3cd57bfd8f25baa2e3251cd55e72f4688a6c7504
Wayne Chen <wenkuchen@gmail.com>
2022-06-19 21:54:15 +0800

    init.h char* enum modified

--- hx711 lcd d1 mini pro/src/init.h
+++ hx711 lcd d1 mini pro/src/init.h
@@ -5,17 +5,11 @@ long CurrADC, LastADC;
 long RefADC, BaseADC; // hx711 scale variables
 float RefKG = 2.0; // Reference weight as RefADC,BaseADC as empty 
 
-enum stateEnum
-{
-    ACTIVE,
-    IDLE
-};
-
-
+enum stateEnum { ACTIVE, IDLE};
+const char* WS_client_type_array[]=['SET_REF','SET_BASE','SET_REFKG', 'SET_UXTIME'];
 enum WS_client_type_enum {SET_REF, SET_BASE, SET_REFKG, SET_UXTIME};
-char* WS_client_type_array[]={"SET_REF", "SET_BASE", "SET_REFKG", "SET_UXTIME"};
 int WS_client_type_array_len = 4;
 enum WS_server_type_enum {SET_REF_OK, SET_BASE_OK, SET_REFKG_OK, SET_UXTIME_OK, ON_CHG} ;
-char* WS_server_type_array[]={"SET_REF_OK", "SET_BASE_OK", "SET_REFKG_OK", "SET_UXTIME_OK", "ON_CHG"};
+const char* WS_server_type_array[]=["SET_REF_OK", "SET_BASE_OK", "SET_REFKG_OK", "SET_UXTIME_OK", "ON_CHG"];
 int WS_server_type_array_len = 5;
 
