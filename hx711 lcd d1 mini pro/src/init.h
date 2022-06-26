18d3d2fac2cb8d2472ab5927480366c5bfa7a8cb
Wayne Chen <wenkuchen@gmail.com>
2022-06-26 10:47:36 +0800

    .......

--- hx711 lcd d1 mini pro/src/init.h
+++ hx711 lcd d1 mini pro/src/init.h
@@ -13,7 +13,7 @@ typedef enum {SET_REF, SET_BASE, SET_REFKG, SET_UXTIME
 } toServer_WStypes_enum;
 typedef enum {ON_CHG,SET_REF_OK, SET_BASE_OK, SET_REFKG_OK, SET_UXTIME_OK
 } toClient_WStypes_enum;
-typedef enum {CURR_ADC,BASE_ADC,REF_ADC,REF_KG
+typedef enum {OP_CODE,CURR_ADC,BASE_ADC,REF_ADC,REF_KG
 } update_scale_WSenum;
 
 #define ToServer_WStypes "SET_REF,SET_BASE,SET_REFKG,SET_UXTIME"
