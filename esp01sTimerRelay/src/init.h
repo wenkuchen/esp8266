cfc31acbe174b798deefa52c7a9dd6db199fffd3
Wayne Chen <wenkuchen@gmail.com>
2022-07-11 17:47:03 +0800

    First config.h init.h main.cpp made

--- esp01sTimerRelay/src/init.h
+++ esp01sTimerRelay/src/init.h
@@ -2,13 +2,8 @@ void esp8266_init();
 void notifyWSclients(const char* );
 
 /*** Global variables and enum of websocket message types between server/client ***/
-extern long CurrADC;
-extern long LastADC;
-extern long RefADC;
-extern long BaseADC; // hx711 scale variables
-extern float RefKG; // Reference weight as RefADC,BaseADC as empty 
 typedef enum {ACTIVE, IDLE
-} scaleStateEnum;
+} timerStateEnum;
 typedef enum {SET_REF, SET_BASE, SET_REFKG, SET_UXTIME
 } toServer_WStypes_enum;
 typedef enum {ON_CHG,SET_REF_OK, SET_BASE_OK, SET_REFKG_OK, SET_UXTIME_OK
