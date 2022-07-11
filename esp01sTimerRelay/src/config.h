f83a064904557af9178a7f497dc407caea06b812
Wayne Chen <wenkuchen@gmail.com>
2022-07-11 17:39:33 +0800

    First config.h init.h main.cpp made

---
+++ esp01sTimerRelay/src/config.h
@@ -0,0 +1,41 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+/*
+ * This example demonstrate how to use asynchronous client & server APIs
+ * in order to establish tcp socket connections in client server manner.
+ * server is running (on port 7051) on one ESP, acts as AP, and other clients running on
+ * remaining ESPs acts as STAs. after connection establishment between server and clients
+ * there is a simple message transfer in every 2s. clients connect to server via it's host name
+ * (in this case 'esp_server') with help of DNS service running on server side.
+ *
+ * Note: default MSS for ESPAsyncTCP is 536 byte and defualt ACK timeout is 5s.
+*/
+
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPmDNS.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#endif
+
+#define DEF_SSID "WKC"
+#define DEF_PASSWORD "23752755"
+
+#define SERVER_HOST_NAME "timer_server"
+
+#define TCP_PORT 7051
+#define DNS_PORT 53
+
+#endif // CONFIG_H
+
+
