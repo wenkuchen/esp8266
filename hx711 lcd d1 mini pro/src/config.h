198148747f7f8277d2daf47603b0e0374358fa82
Wayne Chen <wenkuchen@gmail.com>
2022-07-11 17:19:42 +0800

    .......

--- hx711 lcd d1 mini pro/src/config.h
+++ hx711 lcd d1 mini pro/src/config.h
@@ -3,7 +3,7 @@
 /*
  * This example demonstrate how to use asynchronous client & server APIs
  * in order to establish tcp socket connections in client server manner.
- * server is running (on port 7050) on one ESP, acts as AP, and other clients running on
+ * server is running (on port 7051) on one ESP, acts as AP, and other clients running on
  * remaining ESPs acts as STAs. after connection establishment between server and clients
  * there is a simple message transfer in every 2s. clients connect to server via it's host name
  * (in this case 'esp_server') with help of DNS service running on server side.
@@ -31,7 +31,7 @@
 #define DEF_SSID "WKC"
 #define DEF_PASSWORD "23752755"
 
-#define SERVER_HOST_NAME "timer_server"
+#define SERVER_HOST_NAME "scale_server"
 
 #define TCP_PORT 7051
 #define DNS_PORT 53
