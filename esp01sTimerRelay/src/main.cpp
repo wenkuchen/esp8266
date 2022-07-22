5821d24ed03ee3ebb69fc5422e096943d5ac9141
Wayne Chen <wenkuchen@gmail.com>
2022-07-22 19:40:06 +0800

    main.cpp modified

--- esp01sTimerRelay/src/main.cpp
+++ esp01sTimerRelay/src/main.cpp
@@ -2,10 +2,36 @@
 #include "config.h"
 #include "init.h"
 
+#define RELAY_PIN 3
+
+float prevMarker, nextMarker;
+
+void pins_init(){
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(BUILTIN_LED, HIGH);
+}
+
+void setTimerMarker(char* timerMarkerString){
+
+}
+
+void RunTimer() {
+  // get current time
+  // if current time reaches the next hour marker, 
+  // toggle relay status
+}
+
 void setup() {
-  // put your setup code here, to run once:
+
+  esp8266_init();
+  pins_init();
+
+  //set Unix time when Wifi or websocke connected
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  RunTimer();
 }
